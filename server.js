46f61c05ee5d3b756e8b59bad13934fbad0a76ea
Shelby Monroe <38024477+smon4544@users.noreply.github.com>
2022-06-04 19:03:32 -0500

    query the database to test connection

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -20,7 +21,7 @@ app.use(express.json());
 //   });
 // });
 
-// Connect to database
+// Connect application to the MySQL database
 const db = mysql.createConnection(
   {
     host: '127.0.0.1',
@@ -37,6 +38,14 @@ const db = mysql.createConnection(
 // This route will override all others placed below it, so it needs to be listed as the last one.
 app.use((req, res) => {
   res.status(404).end();
+});
+
+// Queries the database to test the connection -> Should return all the data in the 'candidate' table
+// The [db] object is using the [query()] method, which runs the SQL query and executes the call back with all the resulting rows that matche the query
+// Once this method executes the SQL command, the callback function captures the responses from the query in two variables: the [err], which is the error response, and [rows], which is the database query response
+//  If there are no errors in the SQL query, the [err] value is [null]. This method is the key component that allows SQL commands to be written in a Node.js application.
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 
 // Add the function that will start the Express.js server on port 3001
