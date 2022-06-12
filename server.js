6ee2383e8a03074cb3f99ee79a8212dbadee94e7
Shelby Monroe <38024477+smon4544@users.noreply.github.com>
2022-06-12 16:43:35 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-
-// Imports the mysql2 package installed previously into the node_modules
-const mysql = require('mysql2');
-
 // Imports Express
 const express = require('express');
+// Imports the mysql2 package installed previously into the node_modules
+const mysql = require('mysql2');
+// Import the module
+const inputCheck = require('./utils/inputCheck');
 
 // Adds the PORT designation and the app expression
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// // Creates a GET test route - get() is the chosed route method and res.json is the response method to send the response message 'Hellow World!' back to the client
+// Creates a GET test route - get() is the chosed route method and res.json is the response method to send the response message 'Hellow World!' back to the client
 // app.get('/', (req, res) => {
 //   res.json({
 //     message: 'Hello World'
@@ -34,10 +33,37 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 );
 
-// Default response for any other request (Not Found) - [handles user requests that aren't supported by the app]
-// This route will override all others placed below it, so it needs to be listed as the last one.
-app.use((req, res) => {
-  res.status(404).end();
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// GET or return a signle candidate from the 'candidates' table based on their id
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
 });
 
 // Queries the database to test the connection -> Should return all the data in the 'candidate' table
@@ -48,18 +74,38 @@ app.use((req, res) => {
 //   console.log(rows);
 // });
 
-// GET or return a signle candidate from the 'candidates' table based on their id
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(row);
-// });
-
-// Delete a candidate
+// Creates the API endpoint that will delete a candidate from the databas
 // The DELETE statement has a question mark (?) that denotes a placeholder, making this a prepared statement. A prepared statement can execute the same SQL statements repeatedly using different values in place of the placeholder.
 // //An additional param argument following the prepared statement provides values to use in place of the prepared statement's placeholders. Here, we're hardcoding 1 temporarily to demonstrate how prepared statements work. So this would be the same as saying DELETE FROM candidates WHERE id = 1
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Create a candidate
+// Use the INSERT INTO command for the candidates table to add the values that are assigned to params. The four placeholders must match the four values in params, so we must use an array.
+// Because the candidates table includes four columns—id, first_name, last_name, and industry_connected—we need four placeholders (?) for those four values. The values in the params array must match the order of those placeholders.
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
 //   if (err) {
 //     console.log(err);
 //   }
@@ -67,17 +113,38 @@ app.use((req, res) => {
 // });
 
 // Create a candidate
-// Use the INSERT INTO command for the candidates table to add the values that are assigned to params. The four placeholders must match the four values in params, so we must use an array.
-// Because the candidates table includes four columns—id, first_name, last_name, and industry_connected—we need four placeholders (?) for those four values. The values in the params array must match the order of those placeholders.
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
   }
-  console.log(result);
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Default response for any other request (Not Found) - [handles user requests that aren't supported by the app]
+// This route will override all others placed below it, so it needs to be listed as the last one.
+app.use((req, res) => {
+  res.status(404).end();
 });
 
 // Add the function that will start the Express.js server on port 3001
